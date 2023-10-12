f28c4275657c248ca1cf9573c86f9c87c86d731f
Kim Ho <58868324+KimHo-Kho@users.noreply.github.com>
2023-10-12 13:46:30 -0700

    Update main.py

--- BindMVP/main.py
+++ BindMVP/main.py
@@ -1,84 +1,26 @@
-from map_graph import MapGraph
-from user_input import choose_site
-import random
+from user_input import choose_map
+from bind_map import run_bind_map
+from ascent_map import run_ascent_map
+from split_map import run_split_map
+from lotus_map import run_lotus_map
 
-# Function to generate a random path to a designated site
-def generate_random_path_to_site(site, chosen, map_graph):
-    path = map_graph.get_random_path_to_site(site, chosen)
-    print("Random path to", site)
-    print(" -> ".join(path))
-
-# Main function to set up the map and generate a random path to a site
 def main():
-    # Create the map graph
-    map_graph = MapGraph()
+    while True:
+        selected_map = choose_map()
 
-    # Add locations
-    locations = ["A Short", "B Short", "A Tower", "A Lamps", "A Site",
-                "B Site", "B Garden", "A Bath",
-                "Market", "B Long", "Hookah", "A Lobby", "B Lobby",
-                "B Elbow", "A TP", "B TP",
-                "Attacker Spawn", "Defender Spawn"]
-    for location in locations:
-        map_graph.add_location(location)
+        if selected_map == "Ascent":
+            run_ascent_map()
+        elif selected_map == "Bind":
+            run_bind_map()
+        elif selected_map == "Split":
+            run_split_map()
+        elif selected_map == "Lotus":
+            run_lotus_map()
+        elif selected_map == "gg":
+            break
+        else:
+            print("Functionality for the selected map is not yet implemented.")
 
-    # Add connections with weight 1 and direction
-    connections = [
-        ("A Short", "A TP", False, 2), 
-        ("B Long", "B TP", False, 2), 
-        ("B TP", "A Lobby", False, 1),
-        ("A TP", "B Short", False, 1),
-        ("Attacker Spawn", "A Lobby", False, 1), 
-        ("Attacker Spawn", "Market", False, 1), 
-        ("Attacker Spawn", "B Lobby", False, 1),
 
-        # These connections are bidirectional
-        ("B Lobby", "A Lobby", True, 1),
-        ("A Short", "Market", True, 1), 
-        ("A Short", "A Site", True, 1), 
-        ("A Short", "A Lobby", True, 1),
-        ("B Short", "Hookah", True, 1), 
-        ("B Short", "B Lobby", True, 1), 
-        ("B Short", "Market", True, 1),  
-        ("A Site", "A Tower", True, 1), 
-        ("A Site", "A Lamps", True, 1), 
-        ("A Site", "A Bath", True, 1),
-        ("B Site", "B Elbow", True, 1), 
-        ("B Site", "B Garden", True, 1), 
-        ("B Site", "Hookah", True, 1), 
-        ("B Elbow", "B Site", True, 1),
-        ("Market", "B Short", True, 1), 
-        ("Market", "A Short", True, 1), 
-        ("B Long", "B Garden", True, 1), 
-        ("B Garden", "B Site", True, 1), 
-        ("B Lobby", "B Long", True, 1),
-        ("Defender Spawn", "A Tower", True, 1), 
-        ("Defender Spawn", "B Site", True, 1), 
-        ("Defender Spawn", "B Elbow", True, 1), 
-        ("A Tower", "A Site", True, 1)
-    ]
-
-    for connection in connections:
-        map_graph.add_connection(connection[0], connection[1], connection[2], connection[3])
-
-    # Print the map
-    map_graph.print_map()
-
-    cont = True
-    while cont:
-        # Call the function to get the chosen site
-        chosen_site = choose_site()
-
-        # Call the function to generate a random path to the chosen site
-        starting_points = ["B TP", "B Short", "B Long", "A TP", "A Short"]
-        chosen_starting_point = random.choice(starting_points)
-        print(chosen_starting_point)
-        generate_random_path_to_site(chosen_site, chosen_starting_point, map_graph)
-        
-        # Ask if the user wants to continue
-        user_input = input("Do you want to generate another random path? Press Enter to continue, or type 'gg' to exit: ").strip().lower()
-        cont = (user_input != 'gg')
-
-# Entry point
 if __name__ == "__main__":
     main()
